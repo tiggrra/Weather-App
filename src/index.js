b979ef99a180d598590a839cf5a17bcdda22e37a
tiggrra <78557674+tiggrra@users.noreply.github.com>
2021-03-28 14:10:18 +0200

    added API connection and real data

--- src/index.js
+++ src/index.js
@@ -9,3 +9,35 @@ let weekday = weekdays[now.getDay()];
 
 let h2 = document.querySelector("h2");
 h2.innerHTML = (`${weekday}, ${hour}:${minute}`);
+
+// default location upon page load
+
+let city = "Vienna";
+let apiKey = "01bc9da346c1591ec92736f4f11269b6";
+let apiEndpointCurrent = "https://api.openweathermap.org/data/2.5/weather";
+let units = "metric";
+let apiUrlCurrent = `${apiEndpointCurrent}?q=${city}&units=${units}&appid=${apiKey}`;
+axios.get(apiUrlCurrent).then(displayWeather);
+
+// WEATHER DISPLAY
+
+function displayWeather(response) {
+    
+    // main data
+    let celsiusTemp = Math.round(response.data.main.temp);
+    let celsiusWind = Math.round(response.data.wind.speed*3.6);
+    let currentCity = response.data.name;
+    let currentCountry = response.data.sys.country;
+    let currentCondition = response.data.weather[0].main;
+    let currentHumidity = response.data.main.humidity;
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = (`${currentCity}, ${currentCountry}`);
+    let dispTemp = document.querySelector("#current-temp");
+    dispTemp.innerHTML = (`${celsiusTemp}`);
+    let dispCondition = document.querySelector("#condition");
+    dispCondition.innerHTML = (`${currentCondition}`)
+    let dispHumidity = document.querySelector("#humidity");
+    dispHumidity.innerHTML = (`${currentHumidity}`);
+    let dispWind = document.querySelector("#wind");
+    dispWind.innerHTML = (`${celsiusWind}`);
+}
